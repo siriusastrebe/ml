7c57ee67ea486af164fa6634a105d1d588398cd1
YOUR NAME <siriusastrebe@gmail.com>
2017-09-25 17:37:56 -0700

    Logistic regression using scikitlearn

--- 3/logistic.py
+++ 3/logistic.py
@@ -1,6 +1,8 @@
-# Pages 49-56
+from sklearn.linear_model import LogisticRegression
 from sklearn import datasets
+from plot import plot_decision_regions
 import numpy as np
+
 iris = datasets.load_iris()
 X = iris.data[:, [2, 3]]
 y = iris.target
@@ -14,18 +16,9 @@ sc.fit(X_train)
 X_train_std = sc.transform(X_train)
 X_test_std = sc.transform(X_test)
 
-from sklearn.linear_model import Perceptron
-ppn = Perceptron(n_iter=40, eta0=0.1, random_state=0)
-ppn.fit(X_train_std, y_train)
-
-y_pred = ppn.predict(X_test_std)
-print('Misclassified samples: %d' % (y_test != y_pred).sum())
-
-from sklearn.metrics import accuracy_score
-print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
-
-from plot import plot_decision_regions
+lr = LogisticRegression(C=1000.0, random_state=0)
+lr.fit(X_train_std, y_train)
 
 X_combined_std = np.vstack((X_train_std, X_test_std))
 y_combined = np.hstack((y_train, y_test))
-plot_decision_regions(X=X_combined_std, y=y_combined, classifier=ppn, test_idx=range(105, 150))
+plot_decision_regions(X_combined_std, y_combined, classifier=lr, test_idx=range(105, 150))
--- README.md
+++ README.md
@@ -5,6 +5,15 @@ OS X Python Virtual environments
 ===
 http://www.marinamele.com/2014/07/install-python3-on-mac-os-x-and-use-virtualenv-and-virtualenvwrapper.html
 
+mkvirtualenv ml
+pip install pandas
+pip install sklearn
+pip install numpy
+pip install scipy
+pip install matplotlib
+
 Matplotlib Backend Error
 ===
 https://stackoverflow.com/questions/21784641/installation-issue-with-matplotlib-python
+
+
