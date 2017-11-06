210669f4bf96c3784f9e06120e3ed3a5ae0b005c
YOUR NAME <siriusastrebe@gmail.com>
2017-11-06 02:19:41 -0800

    Chapter 3 decision tree and random forest.

--- 3/logistic.py
+++ 3/logistic.py
@@ -1,5 +1,5 @@
 from sklearn.linear_model import LogisticRegression
-from sklearn import datasets
+rom sklearn import datasets
 from plot import plot_decision_regions
 import numpy as np
 import matplotlib.pyplot as plt
@@ -62,3 +62,22 @@ plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range
 svm = SVC(kernel='rbf', random_state=0, gamma=50, C=1.0)
 svm.fit(X_train_std, y_train)
 plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105, 150))
+
+
+# Page 88 Decision Tree
+from sklearn.tree import DecisionTreeClassifier
+tree = DecisionTreeClassifier(criterion='entropy', max_depth=3, random_state=0)
+tree.fit(X_train, y_train)
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X_combined, y_combined, classifier=tree, test_idx=range(105, 150))
+
+from sklearn.tree import export_graphviz
+export_graphviz(tree, out_file='tree.dot', feature_names=['petal length', 'petal width'])
+
+# Page 91 Random Forest
+from sklearn.ensemble import RandomForestClassifier
+forest = RandomForestClassifier(criterion='entropy', n_estimators=10, random_state=1, n_jobs=2)
+forest.fit(X_train, y_train)
+
+plot_decision_regions(X_combined, y_combined, classifier=forest, test_idx=range(105, 150))
